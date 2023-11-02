8dd48d12fd13ab834b23e74a72473e7c06cae0e1
Agú <enzo.agu@accenture.com>
2023-11-02 16:21:22 -0300

    seccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { MediaPlayerComponent } from './shared/components/media-player/media-player.component';
+import { HeaderUserComponent } from './shared/components/header-user/header-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent,
   ],
   imports: [
     BrowserModule,
